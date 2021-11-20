d134d5c394d8fb1c6124e88f2516e61ec073b831
philosofool <66087178+philosofool@users.noreply.github.com>
2021-11-19 20:59:59 -0700

    Add set up and organize module

---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(name='functionals',
+      version='0.1',
+      description='Functions for functional programming.',
+      url='http://www.github.com/philosofool/functionals',
+      author='S. J. Lenhart',
+      author_email='your.name@example.com',
+      license='MIT',
+      packages=['functionals'],
+      zip_safe=False)
