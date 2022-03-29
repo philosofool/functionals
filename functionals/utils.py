b456d7119471365f98e0daa8678443a4d3b1d5f6
stephen j. lenhart <lenhart.lenhart@gmail.com>
2022-03-29 16:26:16 -0700

    Add compose_function

--- functionals/utils.py
+++ functionals/utils.py
@@ -6,3 +6,13 @@ def nt_builder(name: str, fields) -> namedtuple:
     """Build a named tuple."""
     return namedtuple(name, fields)
     
+def compose_function(*funcs) -> Callable:
+    """Compose sequential function."""
+    def composed(*args, **kwargs):
+        for func in funcs:
+            try:
+                t = func(*t)
+            except NameError:
+                t = func(*args, *kwargs)
+        return t
+    return composed
