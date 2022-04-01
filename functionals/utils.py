c45c108523ab053b5baac9f00ac44c5f484aac4f
stephen j. lenhart <lenhart.lenhart@gmail.com>
2022-04-01 05:39:28 -0700

    Add tests

--- functionals/utils.py
+++ functionals/utils.py
@@ -19,3 +19,17 @@ def explode_list(L):
             yield from explode_list(e)
         else:
             yield e
+
+# tests
+# TODO: move these to a testing file.
+
+def squares(*args):
+    return (x**2 for x in args)
+
+assert sequential_compose(squares, sum, lambda x: x == 36)(3, 3, 3, 3)
+assert sequential_compose(squares, sum, lambda x: x == 25)(3, 4)
+
+assert list(explode_list([1, 2, 3])) == [1, 2, 3]
+assert list(explode_list([1, [2, 3]])) == [1, 2, 3]
+assert list(explode_list([1, 2, "three"])) == [1, 2, "three"]
+assert list(explode_list([[1, [2]], 3])) == [1, 2, 3]
